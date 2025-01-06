751895cb7b6924607fcbd774455a5be62a287496
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2025-01-06 22:19:45 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\"heyy\"y how\"s it going\"");
+
+        int x = 10 ,y= 20;
+        if(x > y){
+            System.out.println("x is greater than y");
+        }else{
+            System.out.println("x is less than y");
+        }
+    }
+}
