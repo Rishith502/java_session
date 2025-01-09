04fb1735988f71491193bdbcf6c9a52f082fabb8
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2025-01-09 11:32:59 +0530

    Add files via upload

---
+++ Day_7.java
@@ -0,0 +1,21 @@
+public class Day_7 {
+    public static void main(String[] args) {
+        //contains Duplicate
+        //[9,4,2,3,1,1]
+        //[2,5,7]
+        
+    }
+}
+
+/*class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){ //i=1 1<4,
+            for(int  j=i+1;j<nums.length;j++){ //3<4,
+                if(nums[i]==nums[j]){ //1=1,
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+} */
