13a914fcff6d800588a28c21d766ff480a768be7
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2025-01-10 12:10:46 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String name;
+    int id;
+    int age;
+    String branch;
+
+    public void makeNoise(){
+        System.out.println("whether teacher is present in the class or notwe have to make noise");
+    }
+
+    public void Study(){
+        System.out.println("Students are studying");
+    }
+}
