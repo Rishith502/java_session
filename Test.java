1813865499d116495d8195d39ff2bfc2bda57e9e
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2025-01-10 12:11:03 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        Student s1=new Student();
+        s1.name = "Rishith";
+        s1.id = 502;
+        s1.age = 19;
+        s1.branch = "CSE";
+        System.err.println(s1.name);
+        System.out.println(s1.id);
+    }
+    
+}
